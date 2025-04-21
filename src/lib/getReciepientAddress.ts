b210eb12a70437472de19633cd6fb8b657702028
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-04-22 00:23:42 +0545

    fix the url and manifest

--- src/lib/getReciepientAddress.ts
+++ src/lib/getReciepientAddress.ts
@@ -1,20 +1,20 @@
 // fetch the profile
+// const PROD = 'https://api.superpa.ge';
+const DEV = 'http://localhost:8000';
+
 export const getRecipientAddress = async (
 	username: string,
 	platform: string
 ) => {
-	const response = await fetch(
-		`http://localhost:8000/api/profile/find/${username}`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				platform: platform,
-			}),
-		}
-	);
+	const response = await fetch(`${DEV}/api/profile/find/${username}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			platform: platform,
+		}),
+	});
 	console.log(response);
 
 	if (!response.ok) {
